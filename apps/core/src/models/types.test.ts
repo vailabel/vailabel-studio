7aa5f4ef25dc5ab08b3253529c39bb9eed72f280
Vichea <nathvichea1@gmail.com>
2025-05-18 20:43:16 -0500

    feat(core) : add test to core

---
+++ apps/core/src/models/types.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "@jest/globals"
+import * as types from "./types"
+
+describe("types", () => {
+  it("should export something", () => {
+    expect(types).toBeDefined()
+  })
+})
