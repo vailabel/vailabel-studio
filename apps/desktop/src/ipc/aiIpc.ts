a6f7d48c36f5434c3deef71b8b01ba2657a8f346
Vichea <nathvichea1@gmail.com>
2025-05-20 14:49:58 -0500

    feat(desktop) : fix python run

--- apps/desktop/src/ipc/aiIpc.ts
+++ apps/desktop/src/ipc/aiIpc.ts
@@ -3,7 +3,7 @@ const path = require("path")
 const { exec, execSync, spawn } = require("child_process")
 import fs from "fs"
 import { dialog } from "electron"
-import { resolveResource } from "../utils"
+import { resolveResource, resolveUnpacked } from "../utils"
 
 export const getPythonPath = () => {
   try {
@@ -107,7 +107,7 @@ ipcMain.handle("get-python-info", async (_event, { pythonPath } = {}) => {
 ipcMain.handle("run-yolo", async (event, data) => {
   const { modelPath, imagePath, pythonPath } = data
 
-  const pythonScript = resolveResource("dist/ai/yolo.py")
+  const pythonScript = resolveUnpacked("dist/ai/yolo.py")
   let cmd = `${pythonPath} "${pythonScript}" --model="${modelPath}" --image-base64="${imagePath}"`
 
   return new Promise((resolve, reject) => {
@@ -148,7 +148,7 @@ ipcMain.handle("run-yolo", async (event, data) => {
 
 ipcMain.handle("install-python-package", async (event, { pythonPath }) => {
   // Check if all requirements are already satisfied
-  const requirementsPath = resolveResource("ai/requirements.txt")
+  const requirementsPath = resolveUnpacked("dist/ai/requirements.txt")
   console.log("Checking requirements at:", requirementsPath)
   try {
     // update pip to the latest version
