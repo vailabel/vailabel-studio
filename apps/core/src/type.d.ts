e0168f7de1d36412715080598d5bbdaf29ba3ece
Vichea <nathvichea1@gmail.com>
2025-05-21 00:03:59 -0500

    chore (core) : fix ipc type

--- apps/core/src/type.d.ts
+++ apps/core/src/type.d.ts
@@ -1,13 +1,7 @@
 type IpcWithEvents = {
   invoke: (channel: string, ...args: any[]) => Promise<any>
-  on?: (
-    channel: string,
-    listener: (event: unknown, ...args: any[]) => void
-  ) => void
-  off?: (
-    channel: string,
-    listener: (event: unknown, ...args: any[]) => void
-  ) => void
+  on: (channel: string, listener: (...args: any[]) => void) => void
+  off: (channel: string, listener: (...args: any[]) => void) => void
 }
 declare global {
   interface Window {
