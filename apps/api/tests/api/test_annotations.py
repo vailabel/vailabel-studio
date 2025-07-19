ec36241484148a292aff36c1e3e35e265fbe733b
Vichea <nathvichea1@gmail.com>
2025-07-19 15:15:39 -0500

    fix(api): fix test and exclude test folder from code scand

--- apps/api/tests/api/test_annotations.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import api.v1.annotations
-
-class TestAnnotations(unittest.TestCase):
-    def test_dummy(self):
-        self.assertTrue(True)
-
-if __name__ == "__main__":
-    unittest.main()
