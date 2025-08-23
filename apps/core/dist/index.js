605cc35680bd3d1d236bdbbe1d9b61fd66fa3654
Vichea <nathvichea1@gmail.com>
2025-08-23 12:18:21 -0500

    fix(build): fix build error

--- apps/core/dist/index.js
+++ apps/core/dist/index.js
@@ -1,36 +1,22 @@
-"use strict"
-Object.defineProperty(exports, "__esModule", { value: true })
-exports.User =
-  exports.Settings =
-  exports.AIModel =
-  exports.ExportFormat =
-  exports.Task =
-  exports.History =
-  exports.Annotation =
-  exports.ImageData =
-  exports.Label =
-  exports.Project =
-  exports.Point =
-    void 0
-class Point {}
-exports.Point = Point
-class Project {}
-exports.Project = Project
-class Label {}
-exports.Label = Label
-class ImageData {}
-exports.ImageData = ImageData
-class Annotation {}
-exports.Annotation = Annotation
-class History {}
-exports.History = History
-class Task {}
-exports.Task = Task
-class ExportFormat {}
-exports.ExportFormat = ExportFormat
-class AIModel {}
-exports.AIModel = AIModel
-class Settings {}
-exports.Settings = Settings
-class User {}
-exports.User = User
+export class Point {
+}
+export class Project {
+}
+export class Label {
+}
+export class ImageData {
+}
+export class Annotation {
+}
+export class History {
+}
+export class Task {
+}
+export class ExportFormat {
+}
+export class AIModel {
+}
+export class Settings {
+}
+export class User {
+}
